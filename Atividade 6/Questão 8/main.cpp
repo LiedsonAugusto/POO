c0d4b23b23d44421aee94d81a53f9194628485fa
Liedson <liedsonaugusto@gmail.com>
2022-06-29 16:30:00 -0300

    Atividades POO

---
+++ Atividade 6/Questão 8/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Pessoa.h"
+
+int main() {
+    Pessoa pessoa1;
+    Pessoa pessoa2("Liedson", 18, 1.75);
+
+    pessoa1.setNome("Fabricio").setIdade(21).setAltura(1.89);
+
+    std::cout << pessoa1.getNome() << " " << pessoa1.getIdade() << " " << pessoa1.getAltura() << std::endl;
+
+    std::cout << pessoa2.getNome() << " " << pessoa2.getIdade() << " " << pessoa2.getAltura() << std::endl;
+
+    return 0;
+}
