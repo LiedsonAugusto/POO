2a2ac95aebedb2767a69d97cfa55b8583ef92d4f
Liedson <liedsonaugusto@gmail.com>
2022-08-07 23:08:51 -0300

    Atividades POO

--- ProjetoBanco/HierarquiaConta/ContaPoupanca/ContaPoupanca.h
+++ ProjetoBanco/HierarquiaConta/ContaPoupanca/ContaPoupanca.h
@@ -8,6 +8,18 @@ class ContaPoupanca : public Conta{
         ContaPoupanca(int, double, const Pessoa &, string);
         //Destrutor
         ~ContaPoupanca();
+        //retorna a data
+        string getData() const {return this->data;}
+
+        virtual void imprimirTransacoes() const;
+        
+        virtual void operator>>(double);
+
+        virtual void extrato() const;
+
+        virtual void adicionaTransacao(const Transacoes &);
+        virtual void removeTransacao();
+        virtual void transferenciaEntreConta(Conta &c, double saldo);
 
 
     private:
